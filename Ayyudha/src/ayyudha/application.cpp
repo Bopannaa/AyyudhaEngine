1dac4893bb1473b97673b65c89c141fcb4325611
Bopanna <bopannaa@gmail.com>
2020-10-30 21:32:22 +0530

    Added event system + basic test.

--- Ayyudha/src/ayyudha/application.cpp
+++ Ayyudha/src/ayyudha/application.cpp
@@ -1,5 +1,8 @@
 #include "application.h"
 
+#include "ayyudha/events/applicationEvent.h"
+#include "ayyudha/log.h"
+
 namespace AA {
 
 	Application::Application()
@@ -13,6 +16,16 @@ namespace AA {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			AA_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			AA_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- Ayyudha/src/ayyudha/core.h
+++ Ayyudha/src/ayyudha/core.h
@@ -13,3 +13,5 @@
 		#define AYYUDHA_API
 	#endif
 #endif
+
+#define BIT(x) (1 << x) 
