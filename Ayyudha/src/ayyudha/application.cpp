d6f5386c63a095a210bfcc6c7720d157efef6b5f
Bopanna <bopannaa@gmail.com>
2020-11-02 20:50:48 +0530

    Added Glad.

--- Ayyudha/src/ayyudha/application.cpp
+++ Ayyudha/src/ayyudha/application.cpp
@@ -1,7 +1,7 @@
 #include "application.h"
 
 #include "ayyudha/events/applicationEvent.h"
-#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 namespace AA
 {
--- Ayyudha/src/platform/windows/windowsWindow.cpp
+++ Ayyudha/src/platform/windows/windowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "ayyudha/events/keyEvent.h"
 #include "ayyudha/events/mouseEvent.h"
 
+#include <glad/glad.h>
+
 namespace AA
 {
 
@@ -52,6 +54,8 @@ namespace AA
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		AA_CORE_ASSERT(status, "Failed to initialize Glad!");
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
