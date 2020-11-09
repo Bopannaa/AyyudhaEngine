bc3c1558db784e1872d9088917474bff595d4f81
Bopanna <bopannaa@gmail.com>
2020-11-09 20:46:59 +0530

    Added AA::Scope and AA::Ref definitions

--- Ayyudha/src/ayyudha/core.h
+++ Ayyudha/src/ayyudha/core.h
@@ -41,3 +41,13 @@
 #define BIT(x) (1 << x) 
 
 #define AA_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1) 
+
+namespace AA {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
