52ad911583d67090217ec8f7e024f0e405306cbd
Bopanna <bopannaa@gmail.com>
2020-11-09 16:55:23 +0530

    Added transforms to Renderer.

--- Ayyudha/src/ayyudha/renderer/renderer.cpp
+++ Ayyudha/src/ayyudha/renderer/renderer.cpp
@@ -3,9 +3,9 @@
 
 namespace AA
 {
-	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
+	Renderer::SceneData *Renderer::s_SceneData = new Renderer::SceneData;
 
-	void Renderer::BeginScene(OrthographicCamera& camera)
+	void Renderer::BeginScene(OrthographicCamera &camera)
 	{
 		s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
 	}
@@ -14,11 +14,11 @@ namespace AA
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader> &shader, const std::shared_ptr<VertexArray> &vertexArray, const glm::mat4 &transform)
 	{
 		shader->Bind();
 		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-
+		shader->UploadUniformMat4("u_Transform", transform);
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
 	}
