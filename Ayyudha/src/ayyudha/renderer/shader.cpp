276bf998088ea88850e66e3c9663ad0561d4eb49
Bopanna <bopannaa@gmail.com>
2020-11-12 11:50:35 +0530

    Shaders can now be read from files
    Change bind target inside OpenGLIndexBuffer constructor
    Made Input a true singleton
    Avoid using std::function in event dispatcher for performance

--- Ayyudha/src/ayyudha/renderer/shader.cpp
+++ Ayyudha/src/ayyudha/renderer/shader.cpp
@@ -3,9 +3,25 @@
 
 #include "renderer.h"
 #include "platform/opengl/openglShader.h"
+#include "ayyudha/application.h"
 
 namespace AA
 {
+	Shader *Shader::Create(const std::string &filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			AA_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(Application::Get().CorrectFilePath(filepath));
+		}
+
+		AA_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader *Shader::Create(const std::string &vertexSrc, const std::string &fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
